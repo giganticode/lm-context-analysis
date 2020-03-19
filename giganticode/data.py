e7f95ddcb8061aa6137be958fca8e4e82c5996a4
hlib <hlibbabii@gmail.com>
2020-03-19 20:52:40 +0200

    fixes

--- giganticode/data.py
+++ giganticode/data.py
@@ -1,14 +1,23 @@
 import collections
 import os
 import pickle
-from typing import List, Union, Optional
+from pathlib import Path
+from typing import List, Optional, Generator
 
 import torch
-from langmodels.cuda_util import get_device
-from langmodels.file_util import get_all_files
 from tqdm import tqdm
 
 from codeprep.api.corpus import nosplit, PreprocessedCorpus
+
+
+def get_all_files(path: str, extension: Optional[str] = 'java') -> Generator[Path, None, None]:
+    if os.path.isfile(path):
+        yield Path(path)
+    else:
+        for root, dirs, files in os.walk(path, followlinks=True):
+            for file in files:
+                if not extension or file.endswith(f'.{extension}'):
+                    yield Path(os.path.join(root, file))
 
 
 class Vocab():
@@ -44,8 +53,7 @@ class Vocab():
 
 
 def numericalize_corpus(path: str, vocab: Vocab,
-                        device: Union[int, str] = get_device(),
-                        output_file: Optional[str] = None) -> torch.Tensor:
+                        output_file: Optional[str] = None) -> torch.LongTensor:
 
     all_files = [f for f in get_all_files(path, None)]
 
@@ -57,7 +65,7 @@ def numericalize_corpus(path: str, vocab: Vocab,
     if output_file:
         with open(output_file, 'w') as f:
             f.write(" ".join(map(lambda x: str(x), all_numbers)))
-    return torch.tensor(all_numbers, device=device, dtype=torch.int64)
+    return torch.LongTensor(all_numbers)
 
 
 class Dictionary(object):
@@ -80,7 +88,7 @@ class Corpus(object):
     def _prep_corpus(self, path: str) -> PreprocessedCorpus:
         base_path, dir = os.path.split(path)
         output_path = os.path.join(base_path, dir + '_prepped')
-        corpus = nosplit(path, no_com=True, no_str=True, no_unicode=True, no_spaces=True, calc_vocab=False, output_path=output_path)
+        corpus = nosplit(path, no_com=True, no_str=True, no_unicode=True, no_spaces=True, calc_vocab=True, output_path=output_path)
         return corpus
 
     def __init__(self, path: str):
